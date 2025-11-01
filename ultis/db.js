fd2156a21006f0cc46d7253ecc6789cb69990a97
khoalololo <minhkhoa278kd@gmail.com>
2025-11-01 21:37:15 +0700

    FIX: deployment on render.com

--- ultis/db.js
+++ ultis/db.js
@@ -1,15 +1,19 @@
 import knex from 'knex';
+import pg from 'pg';
+
+pg.defaults.ssl = process.env.NODE_ENV === 'production'
+  ? { rejectUnauthorized: false }
+  : false;
 
 const db = knex({
   client: 'pg',
-  connection: {
+  connection: process.env.DATABASE_URL || {
     host: 'aws-1-ap-southeast-1.pooler.supabase.com',
     port: 5432,
     user: 'postgres.zrnspwcrbsopkmbalnel',
-    password: '1789', 
+    password: '1789',
     database: 'postgres'
   },
-
   pool: { min: 0, max: 15 }
 });
 
